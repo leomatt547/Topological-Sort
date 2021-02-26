f96f276ef0e3af2512412acaa76d60552ccef6fa
leo.matt.547 <leo.matt.547@gmail.com>
2021-02-27 01:38:10 +0700

    push

--- src/Topological.cpp
+++ src/Topological.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include "Topological.hpp"
 #include "Graph.hpp"
 
@@ -7,45 +8,68 @@ using namespace std;
 
 void topsort(Toposort *T, Graph G){
     int N = GetnumberOfNodes(G);
-    for (int j = 0; j<N ; j++){
+    //cout << GetnumberOfNodes(G) << endl;
+    for (int a = 0; a<N ; a++){
+        Ordering(*T).push_back(vector<int>());
         V(*T).push_back(false);
-        Ordering(*T).push_back(0);
+        for(int b = 0; b < N; b++){
+            Ordering(*T).at(a).push_back(Nil);
+        }
     }
     //cout << "size ordering " << Ordering(*T).size() << endl;
     int i = N - 1;
+    int j = N - 1;
 
     for (int at = 0; at < N; at++){
         if (V(*T).at(at) == false){
+            cout << at << endl;
+            for (int k=0; k < Ordering(*T).size();k++){
+                cout << V(*T).at(k) << " ";
+            }
+            cout<<endl;
             //cout << "V[at] = "<< V(*T).at(at) << endl;
             //cout << "bisa" << endl;
-            i = dfs(i, at, T, G);
+            i = dfs(i, j, at, T, G);
+            j -= 1;
+            i = N - 1;
         }
     }
 }
 
-int dfs(int i, int at,Toposort *T, Graph G){
+int dfs(int i, int j, int at,Toposort *T, Graph G){
     V(*T).at(at) = true;
     //cout << "V["<< at << "] dfs = " << V(*T).at(at) << endl;
-    for (int b = 0; b < Ordering(*T).size(); b++){
+    for (int b = 0; b < (Ordering(*T).size()); b++){
         if (Elmt(G,at,b) == 1){
-            /*for (int k=0; k<Ordering(*T).size();k++){
+            for (int k=0; k < Ordering(*T).size();k++){
                 cout << V(*T).at(k) << ", ";
             }
-            cout<<endl;*/
+            cout<<endl;
             if(V(*T).at(b) == false){
-                //cout << b << endl;
-                i = dfs(i, b, T, G);
+                cout << "b nya adalah "<< b << endl;
+                i = dfs(i, j, b, T, G);
             }
-        }
-    }
-    Ordering(*T).at(i) = at;
+        }  
+    } 
+    Ordering(*T).at(i).at(j) = at;
     return i-1;
 }
 
-vector<string> terjemahkan_key (Toposort *T, map<string,int> matkul){
-    vector<string> hasil;
-    for (int i = 0; i < Ordering(*T).size();i++){
-        hasil.push_back(key_matkulnya_apa(matkul, Ordering(*T).at(i)));
+vector<vector<string>> terjemahkan_key (Toposort *T, map<string,int> matkul){
+    vector<vector<string>> hasil;
+    for (int i=Ordering(*T).size()-1; i>=0; i--){
+        hasil.push_back(vector<string>());
+        for (int j = Ordering(*T).at(i).size()-1; j>=0; j--){
+            //cout << "bisa" << endl;
+            if(Ordering(*T).at(i).at(j)==Nil){
+                break;
+            }else{
+
+                int a = Ordering(*T).size()-1-i;
+                int b = Ordering(*T).at(i).size()-1-j;
+                hasil.at(a).push_back(key_matkulnya_apa(matkul, Ordering(*T).at(i).at(j)));
+            }
+        }
     }
     return hasil;
 }
@@ -53,8 +77,8 @@ int main(int argc, char const *argv[])
 {
     Graph G;
     Toposort T;
-    vector<string> hasil;
-    string a = "prereq.txt";
+    vector<vector<string>> hasil;
+    string a = "../test/prereq.txt";
     vector<string> wow = bacaGraph(a, &G);
     /*
     for (int i=0; i<wow.size(); i++){
@@ -62,23 +86,33 @@ int main(int argc, char const *argv[])
     }
     */
     map<string,int> mapnya = matkul(wow);
-    /*for(auto it = mapnya.cbegin(); it != mapnya.cend(); ++it)
+    for(auto it = mapnya.cbegin(); it != mapnya.cend(); ++it)
     {
         cout << it->first << " " << it->second << endl;
     }
-    */
+    
     makeGraph(&G, mapnya, wow);
     //TulisGraph(&G);
     //cout << endl;
     //cout << GetnumberOfNodes(G) << endl;
     topsort(&T, G);
+    for (int i=0; i< GetnumberOfNodes(G); i++){
+        for (int j=0; j< GetnumberOfNodes(G); j++){
+            cout << Ordering(T).at(i).at(j) << " ";
+        }
+        cout << endl;
+    }
     /*
     for (int i=0; i< GetnumberOfNodes(G); i++){
         cout << Ordering(T).at(i) << endl;
     }*/
     hasil = terjemahkan_key(&T, mapnya);
-    for (int i=0; i< GetnumberOfNodes(G); i++){
-        cout << "Semester "<<i+1<< " : "<<hasil.at(i) << endl;
+    for (int i=0; i< hasil.size(); i++){
+        cout << "Semester "<<i+1<< " : ";
+        for(int j=0; j < hasil.at(i).size(); j++){
+            cout << hasil.at(i).at(j) << ",";
+        }
+        cout << endl;
     }
     return 0;
 }
--- src/Topological.hpp
+++ src/Topological.hpp
@@ -5,15 +5,15 @@
 #include <vector>
 
 typedef struct { 
-    vector<int> ordering;
+    vector<vector<int>> ordering;
     vector<bool> V;
 } Toposort;
 
 #define Ordering(T) (T).ordering
 #define V(T) (T).V
+#define Nil 9999
 
 void topsort(Toposort *T, Graph G);
-int dfs(int i, int at,Toposort *T, Graph G);
-vector<string> terjemahkan_key (Toposort *T);
-vector<string> terjemahkan_key (Toposort *T, map<string,int> matkul);
+int dfs(int i, int j, int at,Toposort *T, Graph G);
+vector<vector<string>> terjemahkan_key (Toposort *T, map<string,int> matkul);
 #endif
